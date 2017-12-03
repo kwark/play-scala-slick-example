b9c5e2f8e6ecb5d764a8d2039115c5d9d39d61f5
Peter Mortier <peter.mortier@gmail.com>
2017-12-03 16:42:26 +0100

    fix play json serialization of Person

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -4,6 +4,7 @@ import play.api.libs.json._
 
 case class Person(id: Long, name: Name, age: Age)
 
-object Person {  
+object Person {
+  import be.venneborg.refined.play.RefinedJsonFormats._
   implicit val personFormat = Json.format[Person]
 }
